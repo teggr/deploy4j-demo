1b13801959b4c8c8c43ee60cfa0bd1eee965068e
teggr <robin@tegg.me.uk>
2024-09-10 20:13:55 +0100

    Initial commit

---
+++ src/main/java/dev/deploy4j/jdemo/Deploy4jDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.deploy4j.jdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deploy4jDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Deploy4jDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/deploy4j/jdemo/Deploy4jDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.deploy4j.jdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deploy4jDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
